ee8421848199349564a806c3d22b72b2ba36e04a
Daniel Dayley <ddayley@qualtrics.com>
2022-09-03 19:08:06 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# home_publisher
+## An automation to update public infrastructure to represent my internal lab
+
+## Usage
+
+```
+
+```
+
+## Installation
+
+1. Run pip3 install home_publisher
+2. Write a systemd job or something I'm not your dev
+3. Profit
+
+## Configuration
+
+Set the following variables when you run this thing:
+
+```
+UNIFI_URL: Everything before the first slash on your unifi controller.
+UNIFI_USERNAME: Your unifi username
+UNIFI_PASSWORD: Your unifi password
+UNIFI_SITE: The name of your unifi site (it's probably 'default')
+CLOUDFLARE_API_KEY: your Cloudflare API token
+CF_DOMAIN: The domain name you are managing with Cloudflare.
+```
+
+Your domain management INI files should look like this :
+
+```
+[servicename]
+port: 1234
+address: 192.168.0.4
+
+[anotherservice]
+port: 4567
+address: 192.168.0.4
+```
+
+Run it like this:
+
+`home_publisher /path/to/file.ini -vv`
+
+## Justification
+
+This is only public because I'm too lazy to clone from a private repo right now.
---
+++ setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup, find_packages
+from setuptools.command.install_scripts import install_scripts
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
+from setuptools.command.build_ext import build_ext
+import subprocess
+import os
+import glob
+
+
+# From https://stackoverflow.com/questions/5932804/set-file-permissions-in-setup-py-file
+# https://blog.niteo.co/setuptools-run-custom-code-in-setup-py/
+def customize(command) :
+	command_name = str(command.mro()[1].__name__).strip()
+	original_run = command.run
+	def run(self) :
+		# Run the rest of the installer first
+		original_run(self)
+		# Create a new subprocess to run the included shell script
+		print("Running " + command_name + " commands...")
+		current_dir_path = os.path.dirname(os.path.realpath(__file__))
+		create_service_script_path = os.path.join(current_dir_path, 'setup.sh')
+		# stdout and stderr are combined in shell output
+		output=subprocess.run([create_service_script_path,command_name],stdout=subprocess.PIPE,stderr=subprocess.STDOUT).stdout
+		print(output.decode('UTF-8'))
+	command.run = run
+	return command
+
+@customize
+class CustomInstallCommand(install) :
+	pass
+
+@customize
+class CustomDevelopCommand(develop) :
+	pass
+
+@customize
+class CustomEggInfoCommand(egg_info) :
+	pass
+
+@customize
+class CustomBuildExtCommand(build_ext) :
+	pass
+
+
+
+setup(name='home_publisher',
+	version='1.0.0',
+	url='',
+	license='Apache2',
+	author='Daniel Dayley',
+	author_email='github@cronocide.com',
+	description='An automation to update public infrastructure to represent my internal lab',
+	packages=find_packages(exclude=['tests']),
+
+	install_requires=['pyyaml',],
+	scripts=['bin/home_publisher'],
+	long_description=open('README.md').read(),
+	
+	zip_safe=True
+)
